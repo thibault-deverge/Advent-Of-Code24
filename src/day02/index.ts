6b5cfbcf4c50cc95489e815890288443b62839aa
thibault <thibault.deverge@gmail.com>
2024-12-03 11:41:49 +0100

    Add Day02 - Part2

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -1,5 +1,6 @@
 import { promises as fs } from "fs";
 
+/* **** Part One **** */
 function isInRange(past: number, current: number, direction: number): boolean {
 	if (direction === 1) {
 		return current > past && current <= past + 3;
@@ -8,8 +9,7 @@ function isInRange(past: number, current: number, direction: number): boolean {
 	}
 }
 
-function isReportSafe(report: string): boolean {
-	const levels = report.split(" ").map((level) => Number(level));
+function isReportSafePart1(levels: number[]): boolean {
 	const direction = levels[0] < levels[1] ? 1 : -1;
 	let pastLevel = levels[0];
 
@@ -23,14 +23,36 @@ function isReportSafe(report: string): boolean {
 	return true;
 }
 
+/* **** Part Two **** */
+function isReportSafePart2(levels: number[]): boolean {
+	if (isReportSafePart1(levels)) {
+		return true;
+	}
+
+	for (let i = 0; i < levels.length; i++) {
+		const newLevels = [...levels.slice(0, i), ...levels.slice(i + 1)];
+		if (isReportSafePart1(newLevels)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/* **** Main **** */
 (async function main() {
 	const fileContent = await fs.readFile("./src/day02/data/input.txt", "utf-8");
 	const reports = fileContent.split("\n");
-	let safeReports = 0;
+	let safeReportsPart1 = 0;
+	let safeReportsPart2 = 0;
 
 	for (const report of reports) {
-		isReportSafe(report) && safeReports++;
+		const reportInt = report.split(" ").map((level) => Number(level));
+
+		isReportSafePart1(reportInt) && safeReportsPart1++;
+		isReportSafePart2(reportInt) && safeReportsPart2++;
 	}
 
-	console.log(safeReports);
+	console.log(safeReportsPart1);
+	console.log(safeReportsPart2);
 })();
