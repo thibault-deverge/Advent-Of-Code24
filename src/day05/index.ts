3adb4baef672bbf69967be4fd0b01eaaf7f7b747
thibault <thibault.deverge@gmail.com>
2024-12-06 01:41:27 +0100

    Add Day05 - Part 1

--- src/day05/index.ts
+++ src/day05/index.ts
@@ -1,8 +1,40 @@
 import { promises as fs } from "fs";
 
+function processPart1(inputRules: string[], inputSequences: string[]): number {
+	let count = 0;
+
+	for (const inputSequence of inputSequences) {
+		const seq = inputSequence.split(",").map(Number);
+		let isValid = true;
+
+		for (const rule of inputRules) {
+			const [first, second] = rule.split("|").map(Number);
+
+			if (seq.includes(first) && seq.includes(second)) {
+				const subSeq = seq.slice(0, seq.indexOf(first));
+
+				if (subSeq.includes(second)) {
+					isValid = false;
+					break;
+				}
+			}
+		}
+
+		isValid && (count += seq[Math.floor(seq.length / 2)]);
+	}
+
+	return count;
+}
+
 /* ***** MAIN ***** */
 (async function main() {
-	const input = (await fs.readFile("src/day05/data/test.txt", "utf-8")).split("\n");
+	const input = (await fs.readFile("src/day05/data/input.txt", "utf-8")).split("\n");
 
-	console.log(input);
+	// Separate the input into two parts
+	const separator = input.indexOf("");
+	const inputRules = input.slice(0, separator);
+	const inputSequences = input.slice(separator + 1);
+
+	// Part1
+	console.log(processPart1(inputRules, inputSequences));
 })();
